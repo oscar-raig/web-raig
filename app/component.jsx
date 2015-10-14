8cf81bcf83e20156f0dd7f8892ec8b05a57d841e
Oscar Raig <oscar.raig@gmail.com>
2015-10-14 21:53:50 +0200

    first step to react

---
+++ app/component.jsx
@@ -0,0 +1,12 @@
+import './main.css';
+import React from 'react';
+
+export default class Hello extends React.Component {
+  render() {
+	return (
+      <div >
+        <a href="http://www.raig.cat">Inici</a>
+      </div>
+    )
+  }
+}
--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,15 @@ var path = require('path');
 
 
 module.exports = {
-    //entry: path.resolve(__dirname, './main.js'),
+    entry: path.resolve(__dirname, './app/main.js'),
     output: {
-        path: path.resolve(__dirname, 'build'),
+        path: path.resolve(__dirname, './'),
         filename: 'bundle.js',
     },
+    module: {
+    loaders: [
+        { test: /\.js$/, loader: 'babel-loader' },
+        { test: /\.css$/, loader: 'style!css' },
+        { test: /\.jsx$/, loader: 'babel' }
+        ]}
 };
-
