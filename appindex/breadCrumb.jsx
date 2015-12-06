ebd4a0e6b9b85d7b1284f948a40a04a6a680eff9
Oscar Raig <oscar.raig@gmail.com>
2015-12-06 22:40:11 +0100

    Adding json to breadcrumbs

--- appindex/breadCrumb.jsx
+++ appindex/breadCrumb.jsx
@@ -3,10 +3,15 @@ import React from 'react';
 
 export default class BreadCrumb extends React.Component {
   render() {
-  
+  	var breadCrumbNodes = this.props.data.map(function(crumb) {
+      return (
+        <a href={crumb.link}>{crumb.linkName} </a>
+      );
+    });
+
 	return (
-      <div >
-        <a href="http://www.raig.cat">Inici</a>
+      <div>
+        {breadCrumbNodes}
       </div>
     )
   }
--- appindex/main.js
+++ appindex/main.js
@@ -6,9 +6,21 @@ import Block_with_list from '../components/block_with_list.jsx'
 main();
 
 function main() {
-    React.render(<BreadCrumb/>, document.getElementById('home'));
-    React.render(<MainTitle title="Benvinguts a la p&agrave;gina  de l Oscar Raig!!!" rootdir="localhost"  imageMainTitle="thumbs/thumb_cami_50.jpg"  />, document.getElementById('principal_header'));
-   	React.render(<Block_with_list href="/personal/personal_index.html"  href_title="Alguna cosa sobre miii	" />, document.getElementById('firstBlock'));
+
+	var dataBreadCrumb = [
+  		{link: "http://www.raig.cat", linkName: "Inici"},
+  		{link: "http://www.raig.cat/personal/personal_index.html", linkName: "Personal"},
+
+	];
+
+    React.render(<BreadCrumb data = {dataBreadCrumb} />, document.getElementById('home'));
+    React.render(<MainTitle title="Benvinguts a la p&agrave;gina  de l Oscar Raig!!!" 
+    	rootdir="localhost"  imageMainTitle="thumbs/thumb_cami_50.jpg"  />, 
+    	document.getElementById('principal_header'));
+   	React.render(<Block_with_list href="/personal/personal_index.html"  
+   		href_title="Alguna cosa sobre miii	" />, 
+   		document.getElementById('firstBlock'));
    
     console.log("Oscar was here!!" + config.rootdirectory);
 }
+ 
